e1f592a3481f53f72d2004ed97115cad0f1af04c
tacevol <2827244094@qq.com>
2021-11-17 16:31:09 +0800

    dlsb first commit

--- src/main/java/com/tacevol/git/GitTest.java
+++ src/main/java/com/tacevol/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!!");
         System.out.println("hello,tacevol!!");
-        System.out.println("fuking korean!!");
+        System.out.println("fuking sbdl!!");
     }
 }
